77ff86a7660850d4660d86a3b46cbb4bc687b1fe
Blvckcodeio <ninokayglobal@gmail.com>
2024-09-16 12:04:09 +0100

    Mad

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,10 @@ import '@/styles/Ourpackages.css'
 import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }) {
-  <Analytics/>
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Component {...pageProps} />
+    <Analytics/>
+    </>
+  );
 }
