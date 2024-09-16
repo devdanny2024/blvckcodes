550611a1f6c68ac252634734404a8ce6275cde25
Blvckcodeio <ninokayglobal@gmail.com>
2024-09-16 12:11:32 +0100

    yh

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,12 +7,14 @@ import '@/styles/Whyus.css'
 import '@/styles/Contactus.css'
 import '@/styles/Ourpackages.css'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   return (
     <>
     <Component {...pageProps} />
     <Analytics/>
+    <SpeedInsights/>
     </>
   );
 }
