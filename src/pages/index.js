65287080f74b2e5efa8bded83b6d84a1aac4732b
Blvckcodeio <ninokayglobal@gmail.com>
2024-09-16 12:15:22 +0100

    yh

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>Blvck Code - Smart Home Solutions</title>
         <meta name="description" content="Enhance your home's capabilities with smart home solutions." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="../images/bc.png" />
       </Head>
       
       <Navbar />
